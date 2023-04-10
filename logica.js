36c9cb29e7d8e22807345a3873f91aec53dca286
Ezequiel Velasco <ezequielvelasco31@gmail.com>
2023-04-10 16:36:01 -0300

    Changes to be committed: 	modified:   ahorcado.html 	new file:   fotos/hangman.png 	new file:   fotos/pixil-frame-0.png 	new file:   fotos/pixil-frame-1.png 	new file:   fotos/pixil-frame-2.png 	new file:   fotos/pixil-frame-3.png 	new file:   fotos/pixil-frame-4.png 	new file:   fotos/pixil-frame-5.png 	new file:   fotos/pixil-frame-6.png 	modified:   logica.js

--- logica.js
+++ logica.js
@@ -1,20 +1,52 @@
     const palabrAdivinar = ingresarPalabra();
+    let aciertos = "";
+
+    
+    
+    //Seteo la variable en ????
+    let palabraOculta="";
+    palabrAdivinar.forEach(caracter =>{
+        palabraOculta= palabraOculta +"?";
+    });
+    
+
 
     let arrCoincidencias = [];
 
     const letra = document.querySelector('input');
-    let intentosRestantes = 6;
+    let intentosRestantes = 5;
     
         letra.oninput = function(){
             if(intentosRestantes != 0){
                 if(soloLetras(letra.value, palabrAdivinar) == true){
                     document.querySelector('input').value="";
-                }else{
-                    document.getElementById("alerta").innerHTML="Quedan "+intentosRestantes+" intentos";
+                    if(palabraOculta == palabrAdivinar.join("")){
+                        document.getElementById("alerta").innerHTML="¡Ganaste!";
+                        document.getElementById("alerta").className="badge bg-success";
+                        
+                        document.querySelector('input').disabled=true;
+                        document.querySelector('input').value="";
+                    }
+                }else{                  
+                    document.querySelector('img').src="fotos/pixil-frame-"+intentosRestantes+".png";
+                    
+                    
                     intentosRestantes = intentosRestantes -1;
+                    if(intentosRestantes == 0){
+                        document.getElementById("alerta").innerHTML="¡Último intento!";
+                    }else{
+                        document.getElementById("alerta").innerHTML="Quedan "+intentosRestantes+" intentos";
+                    }
+                    
+                    
+                    
                 }
             }else{
+                document.querySelector('img').src="fotos/pixil-frame-0.png";
                 document.getElementById("alerta").innerHTML="¡Perdiste!";
+                document.querySelector('input').disabled=true;
+                document.querySelector('input').value="";
+
             }
             
         };
@@ -25,11 +57,11 @@
     function ingresarPalabra(){
         const ventana = prompt("¡Ingresa una palabra para adivinar!");
         const arrPalabra = ventana.split("");
-        console.log(arrPalabra);
+        
 
         document.getElementById("tablero").innerHTML = `
-            <table border="1">
-                <tr>
+            <table class="table" border="1">
+                <tr class="table-secondary">
                     ${creaTablero(arrPalabra)}    
                 </tr>    
             </table>
@@ -44,46 +76,97 @@
         });
         return tablero;
     }
-
+   
     function soloLetras(cadena, palabrAdivinar){
         const pattern = new RegExp('[a-zA-Z]');
-        console.log(pattern.test(cadena));
+        
+        if(getAllIndexes(palabrAdivinar, cadena).length == 0){
+            leyendaCoincidencia(0);
+            document.querySelector('input').value = "";
+            return false;
+        }
+
         if(!pattern.test(cadena)){
             document.querySelector('input').value = "";
             document.getElementById("status").innerHTML = "Solo puedes ingresar letras!!!..";
             return false;
         }else{
             document.getElementById("tablero").innerHTML = `
-            <table border="1">
+            <table class="table" border="1">
                 <tr>
-                    ${buscarCoincidencia(cadena,palabrAdivinar)}    
+                    ${buscarCoincidencia(cadena,palabrAdivinar, palabraOculta)}    
                 </tr>    
             </table>
         `;
             return true;
         }
     }
-
-    function buscarCoincidencia(letra, arrPalabra){
-        let tablero = "";
-        let coincidencias = 0;
-        let contador =0;
-        arrPalabra.forEach(caracter => {
-            if(caracter == letra){
-                tablero = tablero + "<td>"+ caracter +" </td>"; 
-                coincidencias = coincidencias + 1;
-            }else{
-                tablero = tablero + "<td> ? </td>";
-            }
-            leyendaCoincidencia(coincidencias);
-        });
-        return tablero;
+   
+   function getAllIndexes(arr, val) {
+    let indexes = [];
+    
+    for(let i = 0; i < arr.length; i++){
+        if (arr[i] == val){
+            
+            indexes.push(i);
+        }
     }
+    return indexes;
+    }
+   
+    function buscarCoincidencia(letra, arrPalabra){
+        let coincidencias = getAllIndexes(arrPalabra, letra);
+        leyendaCoincidencia(coincidencias.length);
+        let tablero="";
+        if(coincidencias.length == 0){
+            tablero="";
+            
+            for(let i=0;i< palabraOculta.length; i++){
+                if(palabraOculta[i] != "?"){
+                    tablero = tablero + "<td>"+ palabraOculta[i] +" </td>";
+                }else{
+                    tablero = tablero + "<td >"+ palabraOculta[i] +" </td>";
+                }
+                
+            }
+            return tablero;
+        }else{
+            return crearArregloEnBaseA(coincidencias, letra);
+        }
+    }
+
+    function crearArregloEnBaseA(arr,letra){
+        let tablero ="";
+        let pal = palabraOculta.split("");
+
+        let cont=0;
+        arr.forEach(caracter =>{
+            let i=0;
+            pal.forEach(c =>{
+                if(caracter == i){
+                    pal[i]=letra;
+                }
+                i++
+            });
+            cont++
+        });
+        
+        pal.forEach(caracter => {
+            tablero = tablero + "<td>"+ caracter +" </td>";
+        });
+        palabraOculta = pal.join("");
+
+        
+
+        return tablero;
+
+
+       }
 
     function leyendaCoincidencia(coincidencias){
         if(coincidencias > 0){
-            document.getElementById("status").innerHTML = `Hubo ${coincidencias} coincidencias!!!`;
+            document.getElementById("status").innerHTML = `<p class= "bg-success-subtle">Hubo ${coincidencias} coincidencias!!!</p>`;   
         }else{
-            document.getElementById("status").innerHTML = `No hubo coinciencias :(`;
+            document.getElementById("status").innerHTML = `<p class= "bg-danger-subtle">No hubo coinciencias</p>`;      
         }
     }
